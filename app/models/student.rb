5bf2752c27544385279f28c356583a57cb1f729b
Graham Thompson <grwthomps@gmail.com>
2019-11-03 12:45:22 -0700

    Create grade method in student model for calculating grade for specific course

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ApplicationRecord
 
   has_many :course_students
   has_many :courses, through: :course_students
+
+  def grade(course_id)
+    course_students.find_by(course_id: course_id).grade
+  end
 end
