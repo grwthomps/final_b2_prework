2f5a52df7eb5be08c4c92a58d7f3c02bee5de5f0
Graham Thompson <grwthomps@gmail.com>
2019-11-03 13:24:02 -0700

    Create model test for sort_course_students_by_grade method

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe Course, type: :model do
     it {should have_many :course_students}
     it {should have_many(:students).through(:course_students)}
   end
+
+  describe 'instance methods' do
+    it 'can sort course_students by grade descending' do
+    history = Course.create(name: 'History')
+
+    andy = Student.create(name: 'Andy Dwyer')
+    april = Student.create(name: 'April Ludgate')
+    tom = Student.create(name: 'Tom Haverford')
+    leslie = Student.create(name: 'Leslie Knope')
+
+    andy_cs = CourseStudent.create(course_id: history.id, student_id: andy.id, grade: 1.5)
+    april_cs = CourseStudent.create(course_id: history.id, student_id: april.id, grade: 3.4)
+    tom_cs = CourseStudent.create(course_id: history.id, student_id: tom.id, grade: 2.3)
+    leslie_cs = CourseStudent.create(course_id: history.id, student_id: leslie.id, grade: 4.0)
+
+    expect(history.sort_course_students_by_grade).to eq([leslie_cs, april_cs, tom_cs, andy_cs])
+    end
+  end
 end
