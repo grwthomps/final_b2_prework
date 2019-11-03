2ec9534be765d4704f39ccf4cf2725ec9211e198
Graham Thompson <grwthomps@gmail.com>
2019-11-03 12:44:59 -0700

    Create model test for calculating student grade for specific course

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Student, type: :model do
     it {should have_many :course_students}
     it {should have_many(:courses).through(:course_students)}
   end
+
+  describe 'instance methods' do
+    it 'can calculate grade for a specific course' do
+      history = Course.create(name: 'History')
+      andy = Student.create(name: 'Andy Dwyer')
+      CourseStudent.create(course_id: history.id, student_id: andy.id, grade: 2.4)
+
+      expect(andy.grade(history.id)).to eq(2.4)
+    end
+  end
 end
