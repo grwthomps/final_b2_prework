07ce03b82821c30c0dcd38ab4638ed57771bf334
Graham Thompson <grwthomps@gmail.com>
2019-11-04 10:13:32 -0700

    Create class method find_student_name

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,8 @@ class Student < ApplicationRecord
   def grade(course_id)
     course_students.find_by(course_id: course_id).grade
   end
+
+  def self.find_student_name(student_id)
+    find_by(id: student_id).name
+  end
 end
